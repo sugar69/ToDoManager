4e75cfacb4f016c84eb29547b5d841b6a4b650f3
sugar69 <>
2020-09-18 15:21:35 +0900

    add todoeditactivity

--- app/src/main/java/com/example/todomanager/ToDoEditActivity.kt
+++ app/src/main/java/com/example/todomanager/ToDoEditActivity.kt
@@ -7,5 +7,6 @@ class ToDoEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_to_do_edit)
+
     }
 }
